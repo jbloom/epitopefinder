d3be2a250d504d39155f90413fc25642315b9f53
jbloom <jbloom@fhcrc.org>
2014-01-11 22:34:23 -0800

    Initial gh-pages documentation commit

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This package implements epitopefinder.
-
-This package is designed to analyze epitopes from the Immune Epitope Database
-in relation to protein sequences.
-
-Written by Jesse Bloom."""
-
-__version__ = '0.1'
